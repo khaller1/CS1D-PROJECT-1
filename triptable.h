13dafe62c2ad0c3d81ab900b82ce2ccb1b9df378
flocka89 <flocka89@users.noreply.github.com>
2018-03-14 13:29:07 -0700

    Update triptable.h

--- triptable.h
+++ triptable.h
@@ -3,25 +3,27 @@
 
 #include <QAbstractTableModel>
 #include <structs.h>
-
+/** \class TripTable
+ * Class for table displaying a current trip
+ */
 class TripTable : public QAbstractTableModel
 {
     Q_OBJECT
 
 public:
-    explicit TripTable(QVector<Restaurant>& inRestaurant);
+    explicit TripTable(QVector<Restaurant>& inRestaurant); /// Constructor
 
     // Header:
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override; /// Sets header Data
 
     // Basic functionality:
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override; /// Sets row count
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override; /// Sets column count
 
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override; /// Sets table data
 
 private:
-    QVector<Restaurant> r_data;
+    QVector<Restaurant> r_data; /// Restaurant Data
 };
 
 #endif // TRIPTABLE_H
