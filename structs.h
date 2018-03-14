9a72e150fad9de405c66f3427c3b43fd41972b8d
flocka89 <flocka89@users.noreply.github.com>
2018-03-14 13:25:01 -0700

    Update structs.h

--- structs.h
+++ structs.h
@@ -2,38 +2,50 @@
 #define STRUCTS_H
 #include <QString>
 #include <QVector>
-
+/** \struct Menu
+ * Struct Menu definition
+ */
 struct Menu{
-    int baseid;
-    int parentID;
-    QString name;
-    double cost;
+    int baseid; /// Menu IDNum
+    int parentID; /// Parent IDnum
+    QString name; /// Name of item
+    double cost; /// Cost of item
 };
-
+/** \struct AllDist
+ * Struct AllDist definition
+ */
 struct AllDist {
-    int sourceID;
-    int destID;
-    QString destName;
-    double dist;
+    int sourceID; /// Source of distance
+    int destID; /// Destination of distance
+    QString destName; /// Destination Name
+    double dist; /// Distance value
 };
-
+/** \struct AdminData
+ * Struct AdminData definition
+ */
 struct AdminData{
-    QString userName;
-    QString passWord;
+    QString userName; /// Username
+    QString passWord; /// Password
 };
+/** \struct Restaurant
+ * Struct Restaurant definition
+ */
 struct Restaurant{
-    QString name;
-    double SBdist;
-    int menuSize;
-    int idNum;
-    QVector<AllDist> DList;
-    QVector<Menu> RMenu;
+    QString name; /// Restaurant Name
+    double SBdist; /// Distance to Saddleback
+    int menuSize; /// Size of menu
+    int idNum; /// ID number of restaurant
+    QVector<AllDist> DList; /// Distances to other restaurants
+    QVector<Menu> RMenu; /// Menu of restaurant
 };
+/** \struct Purchase
+ * Struct Purchase definition
+ */
 struct Purchase{
-    QString rName;
-    QString pName;
-    double pCost;
-    int qty;
-    double tCost;
+    QString rName; /// Name of restaurant
+    QString pName; /// Name of product
+    double pCost; /// cost of product
+    int qty; /// qty of product 
+    double tCost; /// cost of product*qty
 };
 #endif // STRUCTS_H
