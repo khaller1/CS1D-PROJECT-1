d60ea9ac17eed543c486fe186266f125348188d7
flocka89 <flocka89@users.noreply.github.com>
2018-03-14 13:21:21 -0700

    Update restauranttable.h

--- restauranttable.h
+++ restauranttable.h
@@ -3,23 +3,25 @@
 
 #include <QAbstractTableModel>
 #include <structs.h>
-
+/** \class RestaurantTable
+ * Class for table displaying restaurant values
+ */
 class RestaurantTable : public QAbstractTableModel
 {
     Q_OBJECT
 
 public:
-    explicit RestaurantTable(QVector<Restaurant>& inRestaurant);
+    explicit RestaurantTable(QVector<Restaurant>& inRestaurant); /// Constructor
 
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override; /// Sets header data
 
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override; /// Sets row count
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override; /// Sets column count
 
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override; /// Sets data in table
 
 private:
-    QVector<Restaurant> r_data;
+    QVector<Restaurant> r_data; /// Restaurant Data
 };
 
 #endif // RESTAURANTTABLE_H
