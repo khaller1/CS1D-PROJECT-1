a0ea06cea38b359e9b253aaedcfa1f704968d744
flocka89 <flocka89@users.noreply.github.com>
2018-03-14 13:20:21 -0700

    Update restView.h

--- restView.h
+++ restView.h
@@ -4,7 +4,9 @@
 #include <QDialog>
 #include <main.h>
 #include <QSortFilterProxyModel>
-
+/** \class viewRest
+ * Class for UI element displaying the restaurants available
+ */
 namespace Ui {
 class viewRest;
 }
@@ -14,19 +16,19 @@ class viewRest : public QDialog
     Q_OBJECT
 
 public:
-    explicit viewRest(DataManager* inDM, QWidget *parent = 0);
-    ~viewRest();
+    explicit viewRest(DataManager* inDM, QWidget *parent = 0); /// Constructor
+    ~viewRest(); /// Destructor
 
 private slots:
-    void on_Exit_Button_clicked();
+    void on_Exit_Button_clicked(); ///Returns to previous menu
 
 private:
-    Ui::viewRest *ui;
-    DataManager* DM;
-    RestaurantTable *rTable;
-    QSortFilterProxyModel *Proxy;
-    QVector<Restaurant> R_Data;
-    QWidget *prev;
+    Ui::viewRest *ui; /// UI Element
+    DataManager* DM; /// Data Access
+    RestaurantTable *rTable; /// Restaurant Table
+    QSortFilterProxyModel *Proxy; /// Sorter
+    QVector<Restaurant> R_Data; /// Restaurant Data
+    QWidget *prev; /// Previous Menu
 };
 
 #endif // VIEWREST_H
