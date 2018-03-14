a5e00729cead37a970ea5f7ae0f4e5a116f20fa0
flocka89 <flocka89@users.noreply.github.com>
2018-03-14 13:12:14 -0700

    Update invoice.h

--- invoice.h
+++ invoice.h
@@ -4,6 +4,9 @@
 #include <QDialog>
 #include <structs.h>
 #include <main.h>
+/** \class Invoice
+ * Class for displaying the end result of a trip
+ */
 namespace Ui {
 class Invoice;
 }
@@ -13,20 +16,20 @@ class Invoice : public QDialog
     Q_OBJECT
 
 public:
-    explicit Invoice(DataManager *inDM, QVector<Purchase> &inCart, double TravelDist, QWidget *parent = 0);
-    ~Invoice();
-    void genInvoice();
+    explicit Invoice(DataManager *inDM, QVector<Purchase> &inCart, double TravelDist, QWidget *parent = 0); /// Constructor
+    ~Invoice(); /// Destructor
+    void genInvoice(); /// Generates invoice values
 
 private slots:
-    void on_pushButton_clicked();
+    void on_pushButton_clicked(); /// Closes the invoice window
 
 private:
-    Ui::Invoice *ui;
-    DataManager* DM;
-    QVector<Purchase> trip_Cart;
-    cartTable* C_Table;
-    double tSpent;
-    QWidget* prev;
+    Ui::Invoice *ui; /// UI element
+    DataManager* DM; /// Data Access
+    QVector<Purchase> trip_Cart; /// Cart values
+    cartTable* C_Table; /// Table displaying cart values
+    double tSpent; /// Total value spent
+    QWidget* prev; /// Previous menu
 };
 
 #endif // INVOICE_H
